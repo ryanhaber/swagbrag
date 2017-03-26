ac84ca5f7f1c5f00b11be3b943fa0dff8ea947c9
Ryan Haber <ryan@zoomdata.com>
2017-03-26 12:27:44 -0400

    spiking list functionality

--- js/MainController.js
+++ js/MainController.js
@@ -2,8 +2,32 @@
  * Created by ryan on 3/25/17.
  */
 
+var swaggerDoc;
+
+function inTag(swagDoc, tagName, pathName) {
+   if (tagName in swagDoc[pathName].tags) {
+      return true;
+   } else {
+      return false;
+   }
+}
+
+$.getJSON( "testDocs/swagger.json", function( data ) {
+   console.log("data is: ", data);
+   swaggerDoc = data;
+   var swagPaths = [];
+   for (var key in swaggerDoc.paths) {
+      swagPaths.push(key);
+   }
+   console.log("paths: ", swagPaths);
+
+   // go through the tags and for each tag build a list of the paths that has the tag.
+
+});
+
 var app = angular.module("myApp", []);
 
+
 app.controller('MainController', ['$scope', function($scope) {
-    $scope.stuff = "Hello World!!!";
+    $scope.swagger = swaggerDoc;
 }]);
--- js/dataShaper.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by ryan on 3/25/17.
- */
-
-$.getJSON( "testDocs/swagger.json", function( data ) {
-    console.log("data is: ", data);
-});
-
