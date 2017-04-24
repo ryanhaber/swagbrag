34da035fbf0537cf05a01e28026505b2dbac0130
Ryan Haber <ryan@zoomdata.com>
2017-04-24 00:14:58 -0400

    improves template and wrangles swaggerdoc object kinda

--- js/MainController.js
+++ js/MainController.js
@@ -3,30 +3,61 @@
  */
 
 var swaggerDoc;
+var pathList = [];
+var tagsList = [];
+var pathsByTag = {};
 
-
-$.getJSON( "testDocs/swagger.json", function( data ) {
+$.getJSON( "testDocs/zd24swagger.json", function( data ) {
    console.log("data is: ", data);
    swaggerDoc = data;
 
    // just test building a list of all the path names
-   var pathList = [];
+
    for (var key in swaggerDoc.paths) {
       pathList.push(key);
    }
    console.log("paths: ", pathList);
 
    // go through the tags and for each tag build a list of the paths that has the tag.
-   var tagsList = [];
+
    swaggerDoc.tags.forEach( function(tag){
       tagsList.push(tag.name);
    });
-   console.log("tags: ", tagsList);
+   // console.log("tags: ", tagsList);
 
+
+
+   // fixme: Map the swagger docs from native format to a list of tags, each tag having
+   // fixme: all the path-method-info objects that have that tag
+
+   tagsList.forEach( function(tagToCatalog) {
+      // console.log( "cataloging ", tagToCatalog);
+      var matchingPaths = [];
+      var methodsList = [];
+
+      for (var path in swaggerDoc['paths']) {
+         for (var method in swaggerDoc['paths'][path]) {
+            console.log("path[method]['tags']: ", swaggerDoc['paths'][path][method]['tags']);
+
+            var test = swaggerDoc['paths'][path][method]['tags'].indexOf(tagToCatalog);
+            if (test > -1) {
+               methodsList.push(method);
+            }
+         }
+      }
+
+      if (methodsList != []) {
+         pathsByTag[tagToCatalog] = {'path':path, 'methodLists':methodsList, 'info':swaggerDoc['paths'][path][method]};
+      }
+//      pathsByTag[tagToCatalog] = matchingPaths;
+   });
+
+   console.log("pathsByTag", pathsByTag);
 });
 
-var app = angular.module("myApp", []);
 
+
+var app = angular.module("myApp", []);
 
 app.controller('MainController', ['$scope', function($scope) {
     $scope.swagger = swaggerDoc;
