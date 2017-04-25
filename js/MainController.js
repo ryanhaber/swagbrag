a76eb1221479867a5214c1ccd8a6c74edb5b74dd
Ryan Haber <ryan@zoomdata.com>
2017-04-24 22:44:42 -0400

    fixes transform of swaggerdoc into pathsList

--- js/MainController.js
+++ js/MainController.js
@@ -5,54 +5,45 @@
 var swaggerDoc;
 var pathList = [];
 var tagsList = [];
-var pathsByTag = {};
+var pathsByTag = [];
+
+function tagInList(tag, tagslist) {
+   var retval = tagslist.indexOf(tag);
+   return ( retval );
+}
+
 
 $.getJSON( "testDocs/zd24swagger.json", function( data ) {
    console.log("data is: ", data);
    swaggerDoc = data;
 
-   // just test building a list of all the path names
-
-   for (var key in swaggerDoc.paths) {
-      pathList.push(key);
-   }
-   console.log("paths: ", pathList);
-
    // go through the tags and for each tag build a list of the paths that has the tag.
-
-   swaggerDoc.tags.forEach( function(tag){
+   swaggerDoc.tags.forEach( function(tag) {
       tagsList.push(tag.name);
-   });
-   // console.log("tags: ", tagsList);
+      console.log("tag: ", tag.name);
 
-
-
-   // fixme: Map the swagger docs from native format to a list of tags, each tag having
-   // fixme: all the path-method-info objects that have that tag
-
-   tagsList.forEach( function(tagToCatalog) {
-      // console.log( "cataloging ", tagToCatalog);
-      var matchingPaths = [];
-      var methodsList = [];
+      var tagPath = {'tag':tag.name};
+      var pathList = [];
 
       for (var path in swaggerDoc['paths']) {
          for (var method in swaggerDoc['paths'][path]) {
-            console.log("path[method]['tags']: ", swaggerDoc['paths'][path][method]['tags']);
+            // console.log("Considering method: ", method, ": ", swaggerDoc['paths'][path][method]['tags']);
 
-            var test = swaggerDoc['paths'][path][method]['tags'].indexOf(tagToCatalog);
-            if (test > -1) {
-               methodsList.push(method);
+            if ( tagInList(tag.name, swaggerDoc['paths'][path][method]['tags']) > -1 ) {
+               // console.log("I found it!");
+               var pathInfo = swaggerDoc['paths'][path][method];
+               pathInfo.path = path;
+               pathInfo.method = method;
+               pathList.push(pathInfo);
             }
          }
       }
-
-      if (methodsList != []) {
-         pathsByTag[tagToCatalog] = {'path':path, 'methodLists':methodsList, 'info':swaggerDoc['paths'][path][method]};
-      }
-//      pathsByTag[tagToCatalog] = matchingPaths;
+      tagPath.pathList = pathList;
+      pathsByTag.push(tagPath);
    });
 
-   console.log("pathsByTag", pathsByTag);
+   console.log("pathsByTag: ", pathsByTag);
+
 });
 
 
