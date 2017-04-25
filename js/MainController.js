0b19670683d4a0ee668a596ddc0aa1c9250d0241
Ryan Haber <ryan@zoomdata.com>
2017-04-24 23:10:05 -0400

    adds fxn to retrieve pathList by tag

--- js/MainController.js
+++ js/MainController.js
@@ -13,23 +13,23 @@ function tagInList(tag, tagslist) {
 }
 
 
-$.getJSON( "testDocs/zd24swagger.json", function( data ) {
+$.getJSON("testDocs/zd24swagger.json", function (data) {
    console.log("data is: ", data);
    swaggerDoc = data;
 
    // go through the tags and for each tag build a list of the paths that has the tag.
-   swaggerDoc.tags.forEach( function(tag) {
+   swaggerDoc.tags.forEach(function (tag) {
       tagsList.push(tag.name);
       console.log("tag: ", tag.name);
 
-      var tagPath = {'tag':tag.name};
+      var tagPath = {'tag': tag.name};
       var pathList = [];
 
       for (var path in swaggerDoc['paths']) {
          for (var method in swaggerDoc['paths'][path]) {
             // console.log("Considering method: ", method, ": ", swaggerDoc['paths'][path][method]['tags']);
 
-            if ( tagInList(tag.name, swaggerDoc['paths'][path][method]['tags']) > -1 ) {
+            if (tagInList(tag.name, swaggerDoc['paths'][path][method]['tags']) > -1) {
                // console.log("I found it!");
                var pathInfo = swaggerDoc['paths'][path][method];
                pathInfo.path = path;
@@ -43,13 +43,27 @@ $.getJSON( "testDocs/zd24swagger.json", function( data ) {
    });
 
    console.log("pathsByTag: ", pathsByTag);
-
 });
 
+function getPathListByTag(tagName, pathList) {
+   var retPath;
+   var i = 0;
+
+   pathList.forEach(function (path) {
+      console.log("searching: ", path);
+      if (path.tag == tagName) {
+         console.log("I found a match! ", tagName);
+         retPath = pathList[i];
+      }
+      i++;
+   });
+
+   return(retPath);
+}
 
 
 var app = angular.module("myApp", []);
 
-app.controller('MainController', ['$scope', function($scope) {
-    $scope.swagger = swaggerDoc;
+app.controller('MainController', ['$scope', function ($scope) {
+   $scope.swagger = swaggerDoc;
 }]);
